4333736c33c03c6ae573d013996c9325ae373272
Abhishek Rawat <rawat.abhishek0701@gmai.com>
2024-06-11 11:35:58 +0530

    Task of the day 10/06/2024

---
+++ tasks/Task_01.py
@@ -0,0 +1,13 @@
+# 1.Explain the difference between the = operator and the == operator in Python.
+# '=' assignment operator is used to asigned the value.
+# '==' comparison operator is used to compare the value between two data.
+
+
+# 2.What does the ** operator do in Python, and how is it used?
+# '**' this operator is used to raise to the power of any number
+# Example -  5**2= 25, 6**2= 36
+
+
+# 3.What does the ^ operator do in Python, and in what context is it commonly used?
+# • (a^b) Each bit position in the result is the logical XOR of the bits in the corresponding position of the operands.
+# • 1 if the bits in the operands are different, 0 if they’re equal.
---
+++ tasks/Task_02.py
@@ -0,0 +1,25 @@
+# 1. Develop a Python script that calculates the square and cube of a given number.
+# num = 2 sq - 4, c = 8
+
+num = 2
+square = num**2
+cube = num**3
+print(f"The square of {num} is {square}")
+print(f"The cube of {num} is {cube}")
+
+
+
+# Create a program that takes two numbers as input and prints whether the first number is greater than,
+# less than, or equal to the second number.
+
+Num1 = int(input("Enter the first number"))
+Num2 = int(input("Enter the second number"))
+if Num1 == Num2:
+    print(f"{Num1} is equal to {Num2}")
+elif Num1 < Num2:
+    print(f"{Num1} is less than {Num2}")
+else:
+    print(f"{Num1} is greater than {Num2}")
+
+
+
